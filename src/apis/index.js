9c915cc21bbc0c0adc19ca2f7c4bfe20f04cd3a9
BranchDev110 <steven.fredericks.dev110@gmail.com>
2022-09-21 03:58:20 -0400

    Change HTTP Request to HTTPS

--- src/apis/index.js
+++ src/apis/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_KEY = process.env.REACT_APP_WEATHER_API;
 
 export const fetchWeather = async(city) => {
-    const res = await axios.get(`http://api.openweathermap.org/data/2.5/weather?id=${city}&APPID=${API_KEY}`);
+    const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?id=${city}&APPID=${API_KEY}`);
     console.log(API_KEY);
     return res.data;
 }

bab79b21a2fdb9434d26ca9d9c2b4df70903b07c
BranchDev110 <steven.fredericks.dev110@gmail.com>
2022-09-20 23:21:24 -0400

    Change Some Console

--- src/apis/index.js
+++ src/apis/index.js
@@ -4,6 +4,6 @@ const API_KEY = process.env.REACT_APP_WEATHER_API;
 
 export const fetchWeather = async(city) => {
     const res = await axios.get(`http://api.openweathermap.org/data/2.5/weather?id=${city}&APPID=${API_KEY}`);
-    console.log(res);
+    console.log(API_KEY);
     return res.data;
 }
